089a02304a65839b9baa7dfbe7fcb4c8f72814bc
Liliana Weimer <58522345+lilianaweimer@users.noreply.github.com>
2024-04-20 12:28:04 -0600

    copy changes from local and fix ui bugs

--- coinflip Watch App/ContentView.swift
+++ coinflip Watch App/ContentView.swift
@@ -3,37 +3,96 @@ import SwiftUI
 struct ContentView: View {
     @State private var coin = Coin()
     @StateObject private var sessionStats = CoinflipStats()
+    @State private var isShowingText = true
     
     func flipCoin () -> Void {
         coin.flip()
         sessionStats.addCoinflip(coin: coin)
     }
     
+    var flipAnimation: Animation {
+        Animation.easeInOut(duration: 1)
+    }
+    
     var body: some View {
-        if (coin.wasFlipped()) {
-            Text("It's " + coin.result.description + "!")
-        }
-        
-        if (sessionStats.totalCount > 1) {
-            HStack(
-                alignment: .top,
-                spacing: 15
-            ){
-                HStack {
-                    Image(systemName: "brain.head.profile")
-                    Text(sessionStats.headsCount.description)
+        ZStack() {
+            Circle()
+                .fill(!isShowingText ? Color.white : coin.result == CoinflipResult.Heads ? Color.yellow : Color.orange)
+                .stroke(Color.white, lineWidth:2)
+                .scaleEffect(isShowingText ? 1 : 2.5)
+                .animation(
+                    flipAnimation,
+                    value: coin.wasFlipped()
+                )
+                .blur(radius: (1) * 20)
+                .frame(width: .infinity, height: .infinity)
+                .padding(.top, -20)
+            
+            
+            VStack() {
+                HStack(
+                    alignment: .top,
+                    spacing: 15
+                ) {
+                    if (isShowingText) {
+                        if (sessionStats.totalCount > 1) {
+                            VStack {
+                                Image(systemName: "brain.head.profile")
+                                    .accessibilityIdentifier("TotalHeadsCountIcon")
+                                Text(sessionStats.headsCount.description)
+                                    .accessibilityIdentifier("TotalHeadsCount")
+                            }
+                        }
+                        
+                        if (coin.wasFlipped()) {
+                            Text("It's " + coin.result.description + "!")
+                                .accessibilityIdentifier("CoinflipResult")
+                        }
+                        
+                        if (sessionStats.totalCount > 1) {
+                            VStack {
+                                Image(systemName: "cat")
+                                    .accessibilityIdentifier("TotalTailsCountIcon")
+                                Text(sessionStats.tailsCount.description)
+                                    .accessibilityIdentifier("TotalTailsCount")
+                            }
+                        }
+                        
+                    }
+                    
+                }.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+                
+                Spacer(minLength: 20)
+                
+                if (isShowingText) {
+                    Text(coin.result == CoinflipResult.Heads ? Image(systemName: "brain.head.profile") : Image(systemName: "cat"))
+                        .font(.title)
+                        .opacity(1)
+                        .frame(alignment: .center)
+                        .accessibilityIdentifier("CoinflipResultIcon")
                 }
-                HStack {
-                    Image(systemName: "cat")
-                    Text(sessionStats.tailsCount.description)
+                
+                Spacer(minLength: 20)
+                
+                if (isShowingText) {
+                    Button(action: {
+                        withAnimation {
+                            isShowingText.toggle()
+                        }
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                            withAnimation {
+                                isShowingText.toggle()
+                            }
+                        }
+                        flipCoin()
+                    }, label: {
+                        Text("Flip a coin")
+                            .accessibilityIdentifier("FlipCoinButton")
+                    }).frame(maxHeight: .infinity, alignment: .bottom)
+                    
                 }
-            }.frame(maxHeight: .infinity, alignment: .bottom)
-
+            }
         }
-        
-        Button(action: flipCoin, label: {
-            Text("Flip a coin")
-        }).frame(maxHeight: .infinity, alignment: .bottom)
     }
 }
 
